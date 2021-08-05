4862b34585891247c222fc9980b7eff94b1f301c
Danny Roubin <dannyroubin1@gmail.com>
2021-08-05 13:44:00 -0700

    updated db.js file to work for this project

--- public/db.js
+++ public/db.js
@@ -1,11 +1,11 @@
 let db;
-let budgetVersion;
+let workoutVersion;
 
-// Create a new db request for a "budget" database.
-const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+// Create a new db request for a "workout" database.
+const request = indexedDB.open("workout", workoutVersion || 21);
 
 request.onupgradeneeded = function (e) {
-  console.log('Upgrade needed in IndexDB');
+  console.log("Upgrade needed in IndexDB");
 
   const { oldVersion } = e;
   const newVersion = e.newVersion || db.version;
@@ -15,7 +15,7 @@ request.onupgradeneeded = function (e) {
   db = e.target.result;
 
   if (db.objectStoreNames.length === 0) {
-    db.createObjectStore('BudgetStore', { autoIncrement: true });
+    db.createObjectStore("workoutStore", { autoIncrement: true });
   }
 };
 
@@ -24,13 +24,13 @@ request.onerror = function (e) {
 };
 
 function checkDatabase() {
-  console.log('check db invoked');
+  console.log("check db invoked");
 
-  // Open a transaction on your BudgetStore db
-  let transaction = db.transaction(['BudgetStore'], 'readwrite');
+  // Open a transaction on your workoutStore db
+  let transaction = db.transaction(["workoutStore"], "readwrite");
 
-  // access your BudgetStore object
-  const store = transaction.objectStore('BudgetStore');
+  // access your workoutStore object
+  const store = transaction.objectStore("workoutStore");
 
   // Get all records from store and set to a variable
   const getAll = store.getAll();
@@ -39,27 +39,27 @@ function checkDatabase() {
   getAll.onsuccess = function () {
     // If there are items in the store, we need to bulk add them when we are back online
     if (getAll.result.length > 0) {
-      fetch('/api/transaction/bulk', {
-        method: 'POST',
+      fetch("/api/transaction/bulk", {
+        method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
         },
       })
         .then((response) => response.json())
         .then((res) => {
           // If our returned response is not empty
           if (res.length !== 0) {
-            // Open another transaction to BudgetStore with the ability to read and write
-            transaction = db.transaction(['BudgetStore'], 'readwrite');
+            // Open another transaction to workoutStore with the ability to read and write
+            transaction = db.transaction(["workoutStore"], "readwrite");
 
             // Assign the current store to a variable
-            const currentStore = transaction.objectStore('BudgetStore');
+            const currentStore = transaction.objectStore("workoutStore");
 
             // Clear existing entries because our bulk add was successful
             currentStore.clear();
-            console.log('Clearing store 🧹');
+            console.log("Clearing store 🧹");
           }
         });
     }
@@ -67,27 +67,27 @@ function checkDatabase() {
 }
 
 request.onsuccess = function (e) {
-  console.log('success');
+  console.log("success");
   db = e.target.result;
 
   // Check if app is online before reading from db
   if (navigator.onLine) {
-    console.log('Backend online! 🗄️');
+    console.log("Backend online! 🗄️");
     checkDatabase();
   }
 };
 
 const saveRecord = (record) => {
-  console.log('Save record invoked');
-  // Create a transaction on the BudgetStore db with readwrite access
-  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  console.log("Save record invoked");
+  // Create a transaction on the workoutStore db with readwrite access
+  const transaction = db.transaction(["workoutStore"], "readwrite");
 
-  // Access your BudgetStore object store
-  const store = transaction.objectStore('BudgetStore');
+  // Access your workoutStore object store
+  const store = transaction.objectStore("workoutStore");
 
   // Add record to your store with add method.
   store.add(record);
 };
 
 // Listen for app coming back online
-window.addEventListener('online', checkDatabase);
+window.addEventListener("online", checkDatabase);
