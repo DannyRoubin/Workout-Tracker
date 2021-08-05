37fe52e9c300d2d951bb2d5b13d28f834396b3a0
Danny Roubin <dannyroubin1@gmail.com>
2021-08-05 13:34:28 -0700

    user is now able to navigate to exercise and stats routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,6 +18,14 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
