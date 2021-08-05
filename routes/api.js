7bc2271d8521065d40f10461e2d88375b1f1c525
Danny Roubin <dannyroubin1@gmail.com>
2021-08-05 14:05:45 -0700

    finally got it so you view your last workout on the home page

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,23 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ date: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 module.exports = router;
